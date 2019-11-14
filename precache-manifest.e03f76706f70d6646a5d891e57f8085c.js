525ce2dc71a7d4b2acb13a6dd3484e729dc59303
almeidarenato <renatoam@ymail.com>
2019-11-14 11:27:27 -0300

    Updates

---
+++ precache-manifest.e03f76706f70d6646a5d891e57f8085c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93220185f6c285867d5d28e4cf3e2daf",
+    "url": "/react-bootstrap/index.html"
+  },
+  {
+    "revision": "7320d9bc4a5c978474ff",
+    "url": "/react-bootstrap/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9ef87e223e5d648ec52f",
+    "url": "/react-bootstrap/static/js/2.dbd5b2b9.chunk.js"
+  },
+  {
+    "revision": "7320d9bc4a5c978474ff",
+    "url": "/react-bootstrap/static/js/main.c4ae3617.chunk.js"
+  },
+  {
+    "revision": "63a25382fa6f386e6d28",
+    "url": "/react-bootstrap/static/js/runtime-main.c4b5c88e.js"
+  },
+  {
+    "revision": "d61546cda03af82f672458cb776c67a1",
+    "url": "/react-bootstrap/static/media/boatImage.d61546cd.jpg"
+  }
+]);
