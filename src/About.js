3394ac806e42d996aaf132629d82c581f0e3aeee
almeidarenato <renatoam@ymail.com>
2019-11-14 11:23:52 -0300

    initial commit

---
+++ src/About.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+export const About = () => (
+  <div>
+    <h2>About Page</h2>
+    <p>
+      Caros amigos, o novo modelo estrutural aqui preconizado apresenta
+      tendências no sentido de aprovar a manutenção das direções preferenciais
+      no sentido do progresso.
+    </p>
+    <p>
+      A certificação de metodologias que nos auxiliam a lidar com o julgamento
+      imparcial das eventualidades pode nos levar a considerar a reestruturação
+      do orçamento setorial. Não obstante, a contínua expansão de nossa
+      atividade acarreta um processo de reformulação e modernização do impacto
+      na agilidade decisória. No entanto, não podemos esquecer que a estrutura
+      atual da organização auxilia a preparação e a composição do levantamento
+      das variáveis envolvidas. Do mesmo modo, a necessidade de renovação
+      processual garante a contribuição de um grupo importante na determinação
+      de alternativas às soluções ortodoxas.
+    </p>
+  </div>
+);
---
+++ src/NoMatch.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+export const NoMatch = () => (
+  <div>
+    <h2>NoMatch Page</h2>
+    <p>Infelizmente não encontramos oque estava procurando :(</p>
+  </div>
+);
